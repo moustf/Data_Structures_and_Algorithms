6665650ce7df4e34e6dcda0fb8b0e382580641ce
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-09 17:52:01 +0200

    Added the solution to contains duplicates leetcode problem using new set builtin data structure with n time complexity

--- arrays/leetcode/containsDuplicates.ts
+++ arrays/leetcode/containsDuplicates.ts
@@ -7,3 +7,16 @@ function containsDuplicateOne(nums: number[]): boolean {
 
   return false;
 };
+
+function containsDuplicateTwo(nums: number[]): boolean {
+  const unique = new Set<number>();
+
+  for (let i = 0; i < nums.length; i++) {
+      if (unique.has(nums[i])) {
+          return true;
+      }
+      unique.add(nums[i]);
+  }
+
+  return false;
+};
