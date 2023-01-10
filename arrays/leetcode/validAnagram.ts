c5030102d615706a43e06cc0776f5dc7eef0ebe3
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-10 16:27:24 +0200

    Added the solution to valid anagram leetcode problem using traversing the string and removing characters technique

--- arrays/leetcode/validAnagram.ts
+++ arrays/leetcode/validAnagram.ts
@@ -1,3 +1,16 @@
 function isAnagramOne(s: string, t: string): boolean {
   return s.split('').sort().join('') === t.split('').sort().join('');
 };
+
+function isAnagramTwo(s: string, t: string): boolean {
+  if (s.length !== t.length) return false;
+
+  for (let i = 0; i < s.length; i++) {
+      if (t.includes(s.charAt(i))) {
+          t = t.replace(s.charAt(i), '');
+      }
+  }
+
+  if (t.length === 0) return true;
+  return false;
+};
