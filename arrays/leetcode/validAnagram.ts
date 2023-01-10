ffb012f92950f1e0f841e492998e59d1ecb0e768
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-10 16:28:32 +0200

    Added the solution to valid anagram leetcode problem using object store and number of occurences in the strings

--- arrays/leetcode/validAnagram.ts
+++ arrays/leetcode/validAnagram.ts
@@ -14,3 +14,28 @@ function isAnagramTwo(s: string, t: string): boolean {
   if (t.length === 0) return true;
   return false;
 };
+
+function isAnagramThree(s: string, t: string): boolean {
+  if (s.length !== t.length) return false;
+
+  const store = {};
+
+  for (let c of s) {
+      if (!(c in store)) {
+          store[c] = 1
+      } else {
+          store[c] += 1;
+      }
+  }
+
+  for (let c of t) {
+      if (!(c in store)) {
+          return false;
+      } else {
+          store[c] -= 1;
+          if (store[c] < 0) return false;
+      }
+  }
+
+  return true;
+};
