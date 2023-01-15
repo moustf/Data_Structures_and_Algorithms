f89c1acf2627e4daf46d92a99c48d94ea3e5b306
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-15 21:55:36 +0200

    Added the solution to longest consecutives sequence using traversing the array and maxScore and score pointers and set data structure

--- arrays/leetcode/longestConsecutiveSequence.ts
+++ arrays/leetcode/longestConsecutiveSequence.ts
@@ -22,3 +22,25 @@ const search = (nums: number[]): number => {
 const longestConsecutiveOne = (nums: number[]): number => (
   nums.length === 0 ? 0 : search(nums.sort((a, b) => a - b))
 );
+
+const longestConsecutiveTwo = (nums: number[]): number => {
+  const numsSet = new Set(nums);
+  let maxScore = 0;
+
+  for (const num of [...numsSet]) {
+      const prev = num - 1;
+
+      if (numsSet.has(prev)) continue;
+
+      let [curr, score] = [num, 1];
+
+      const isStreak = () => numsSet.has(curr + 1);
+      while(isStreak()) {
+          curr++;
+          score++;
+      }
+      maxScore = Math.max(maxScore, score);
+  }
+
+  return maxScore;
+};
