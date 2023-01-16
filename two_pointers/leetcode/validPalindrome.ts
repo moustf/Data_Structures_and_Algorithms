2adee7ebf2312ec60377ca1def7d654003143d7d
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-16 12:42:32 +0200

    Added the solution to valid plaindrome leetcode solution using two pointers technique and regex

--- two_pointers/leetcode/validPalindrome.ts
+++ two_pointers/leetcode/validPalindrome.ts
@@ -5,3 +5,32 @@ const filterSymbols = (s: string): string => (
 const isPalindromeOne = (s: string): boolean  => (
     filterSymbols(s) === filterSymbols(s).split('').reverse().join('') 
 );
+
+const isPalindromeTwo = (s: string): boolean => {
+    if (s.length <= 1) return true;
+
+    const regex = /[a-zA-Z0-9]/;
+    let [left, right] = [0, s.length - 1];
+    let leftChar: string;
+    let rightChar: string;
+
+    while (left < right) {
+        leftChar = s.charAt(left);
+        rightChar = s.charAt(right);
+
+        if (!regex.test(leftChar)) {
+            left += 1;
+        } else if (!regex.test(rightChar)) {
+            right -= 1;
+        } else {
+            if (leftChar.toLowerCase() !== rightChar.toLowerCase()) {
+                return false;
+            }
+
+            left += 1;
+            right -= 1;
+        }
+    }
+
+    return true;
+};
