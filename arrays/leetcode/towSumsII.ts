4564ce7acf0c2de45c62c6cf773b4c688c041093
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-17 12:55:44 +0200

    Solving the two sum two leetcode problem using brute code solution and helper function

--- arrays/leetcode/towSumsII.ts
+++ arrays/leetcode/towSumsII.ts
@@ -7,3 +7,25 @@ function twoSumOne(numbers: number[], target: number): number[] {
         }
     }
 };
+
+// ? ----------------------------------------------------------
+
+const findSum = (nums: number[], target: number, current: number, ind: number): number => {
+  for (let i = 0; i < nums.length; i++) {
+      if (i === ind) continue;
+      if (nums[i] + current === target) {
+          return i + 1;
+      }
+  }
+
+  return -1;
+}
+
+function twoSumTwo(numbers: number[], target: number): number[] {
+  for (let i = 0; i < numbers.length; i++) {
+      const otherInd = findSum(numbers, target, numbers[i], i);
+      if (otherInd !== -1) {
+          return [i + 1, otherInd];
+      }
+  }
+};
