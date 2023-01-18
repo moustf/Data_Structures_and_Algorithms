a35f039c2371f6345a752ec0b417c6b5200c332b
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-19 00:29:04 +0200

    Added the solution to container with most water leetcode solution using two pointers technique

--- two_pointers/leetcode/containerWithMostWater.ts
+++ two_pointers/leetcode/containerWithMostWater.ts
@@ -11,3 +11,22 @@ function maxAreaOne(height: number[]): number {
 
   return res;
 };
+
+function maxArea(height: number[]): number {
+  let [l, r] = [0, height.length - 1];
+  let res = 0;
+
+  while (l < r) {
+      let area = (r - l) * Math.min(height[l], height[r]);
+      res = Math.max(res, area);
+      if (height[r] > height[l]) {
+          l += 1;
+      } else if (height[r] < height[l]) { // ?  we can omit this.
+          r -= 1;
+      } else {
+          r -= 1;
+      }
+  }
+
+  return res;
+};
