95a6b1280207a7a98c6a1f25b2299c93eb22b6d8
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-22 02:00:55 +0200

    Added the solution to best time to buy a stock leetcode problem using sliding window techniqu

--- sliding_window/leetcode/bestTimeToBuySellStock.ts
+++ sliding_window/leetcode/bestTimeToBuySellStock.ts
@@ -15,3 +15,19 @@ function maxProfitOne(prices: number[]): number {
 
   return val;
 };
+
+function maxProfitTwo(prices: number[]): number {
+  let [l, r, val] = [0, 1, 0];
+
+  while (r <= prices.length - 1){
+      const canSlide = prices[r] <= prices[l];
+      if (canSlide) l = r;
+
+      const window = prices[r] - prices[l];
+
+      val = Math.max(val, window);
+      r += 1;
+  }
+
+  return val;
+};
