c6c29ac8f5d85641e801ad88dfd1c08a4f5b1134
Mustafa Salem <mustafaissasalem@gmail.com>
2023-02-03 18:15:54 +0200

    Added the solution to zigzag conversion leetcode problem using the pattern that zig zag takes to arrange the characters and brute force approach

--- arrays/leetcode/zigzagConversion.ts
+++ arrays/leetcode/zigzagConversion.ts
@@ -35,3 +35,39 @@ const convertOne = (s: string, numRows: number): string => {
     .join("")
     .replace(/\s/g, "");
 };
+
+
+// ! The followed pattern in zig zag approach:
+// ? First chars in each section jumps 8 chars to take the first char of the next section.
+// ? The second char jumps 6 chars then 2 chars to hoop into the next section.
+// ? The third char moves 4 chars.
+// ? The forth element jumps 2 chars and 6 other chars to enter the next section.
+// ? The fifth element jumps 8 chars each time.
+// ? And so on ...
+const convertTwo = (s: string, numRows: number): string => {
+  if (numRows === 1) return s;
+  let [ans, n, charsInSection] = ["", s.length, 2 * (numRows - 1)];
+
+  for (let cr = 0; cr < numRows; cr++) {
+    // cr = current row
+    let ind = cr;
+    while (ind < n) {
+      ans += s[ind];
+
+      // If current row is not first or last,
+      // then we have to add one more character of current section.
+      if (cr !== 0 && cr !== numRows - 1) {
+        let charsInBetween = charsInSection - 2 * cr;
+        let secondInd = ind + charsInBetween;
+
+        if (secondInd < n) {
+          ans += s[secondInd];
+        }
+      }
+
+      ind += charsInSection;
+    }
+  }
+
+  return ans;
+};
