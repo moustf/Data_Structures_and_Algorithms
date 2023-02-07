4c7d7538ac41472f87f28186f235386a054248ef
Mustafa Salem <mustafaissasalem@gmail.com>
2023-02-07 11:45:56 +0200

    Added the solution to fruits into baskets leetcode problem using an optimzed brute force technique with time complexity of n squared

--- hash_tables_and_maps/leetcode/fruitIntoBaskets.ts
+++ hash_tables_and_maps/leetcode/fruitIntoBaskets.ts
@@ -15,3 +15,27 @@ function totalFruitOne(fruits: number[]): number {
 
   return max;
 };
+
+// ! Optimized Brute Force. Time Out in some scenarios!!
+// ? Time Complexity: O(n^2), Space Complexity: O(1).
+function totalFruitTwo(fruits: number[]): number {
+  let [max, n] = [0, fruits.length];
+
+  for (let l = 0; l < n; l++) {
+    const basket = new Set<number>();
+    let r = l;
+
+    while (r < n) {
+      // ? Early stop. If adding this fuit makes 3 types fo fruit, we should stop the inner loop.
+      if (!basket.has(fruits[r]) && basket.size === 2) break;
+
+      // ? Otherwise, update the number of this fruit.
+      basket.add(fruits[r]);
+      r += 1;
+    }
+
+    max = Math.max(max, r - l);
+  }
+
+  return max;
+};
